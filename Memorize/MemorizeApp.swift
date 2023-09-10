f67b02b040f7bcf1fdc6dceedbcf7f8ff91d0e98
chernOS <chern6998@163.com>
2023-09-11 03:45:54 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by chern23 on 11/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
